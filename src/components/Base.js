258b911fb1725b7d2c9b76d071358e84aab302b6
Pratyaksh Saxena <pratyakshsaxena88@gmail.com>
2020-08-16 17:10:11 +0530

    first commit

---
+++ src/components/Base.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { ReactComponent as TeamIcon } from "./team.svg";
+import { ReactComponent as EventIcon } from "./event.svg";
+
+const Base = ({ className = "text-dark p-4", children, history }) => {
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function myFunction2() {
+    console.log(2);
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  return (
+    <div className="container-fluid">
+      <div className="row" style={{ backgroundColor: "black" }}>
+        <div className="col-12">
+          <div className="topnav" id="myTopnav">
+            <a style={{ marginLeft: "-20px" }}>
+              <img
+                title="Home"
+                src={require("./black.png")}
+                alt="LOGO"
+                style={{ maxHeight: "80px", cursor: "pointer" }}
+                onClick={() => {
+                  history.push("/");
+                }}
+              />
+            </a>
+            <a href="" className="pl-4" style={{ paddingTop: "28px" }}>
+              <TeamIcon /> Our Team
+            </a>
+
+            <div className="dropdown pl-2" style={{ paddingTop: "16px" }}>
+              <button className="dropbtn" onClick={myFunction2}>
+                <EventIcon />
+                &nbsp;&nbsp;Events <i className="fa fa-caret-down"></i>
+              </button>
+              <div className="dropdown-content" id="myDropdown">
+                <a href="" className="ml-3" style={{ fontSize: "15px" }}>
+                  2019
+                </a>
+              </div>
+            </div>
+            <a
+              style={{ fontSize: "20px", color: "white" }}
+              className="icon pt-4"
+              onClick={() => {
+                myFunction();
+              }}
+            >
+              &#9776;
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={className}>{children}</div>
+    </div>
+  );
+};
+
+export default withRouter(Base);
---
+++ src/core/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Base from "../components/Base";
+
+function Home() {
+  return <Base>HOME</Base>;
+}
+
+export default Home;
